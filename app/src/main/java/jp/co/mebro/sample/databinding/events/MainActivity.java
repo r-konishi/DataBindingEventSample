402f1d8c65008a431b283c164782d1f81dc4159f
r-konishi <r.konishi@developer.mebro.co.jp>
2016-05-11 12:16:11 +0900

    gradle で DataBinding を有効にして、layout と Activity の Class を修正

--- app/src/main/java/jp/co/mebro/sample/databinding/events/MainActivity.java
+++ app/src/main/java/jp/co/mebro/sample/databinding/events/MainActivity.java
@@ -1,13 +1,17 @@
 package jp.co.mebro.sample.databinding.events;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import jp.co.mebro.sample.databinding.events.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding mActivityMainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
